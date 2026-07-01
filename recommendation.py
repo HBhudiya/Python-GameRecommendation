29d60e3791f41ddbaa17c6c6d729d31d1a25f6c7
HBhudiya <h.bhudiya@outlook.com>
2026-07-01 15:39:12 +0100

    Implement Game Recommendation Function Using RAWG API

--- recommendation.py
+++ recommendation.py
@@ -7,3 +7,19 @@ load_dotenv()
 
 API_KEY = os.getenv("RAWG_API_KEY")
 BASE_URL = "https://api.rawg.io/api"
+
+# Function to output recommendations based on a game name or keyword
+def keyword(query):
+    url = f"{BASE_URL}/games"
+    parameters = {
+        "key": API_KEY,
+        "search": query,
+        "page_size": 5
+    }
+
+    # Make a GET request to the RAWG API    
+    response = requests.get(url, params=parameters)
+    data = response.json()
+
+    # Return the list of recommended games
+    return data.get("results", [])
